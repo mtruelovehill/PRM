003e66d89139c948b0bc6e9f501b2702c4e31eb6
Monica Truelove-Hill <m.truelovehill@ed.ac.uk>
2024-10-28 17:19:46 +0000

    wk 7 lab updates

---
+++ Labs/ggplotgui_for_positcloud.R
@@ -0,0 +1,14 @@
+## R code for launching ggplotgui on own device
+## To run code use `ctrl` + `enter` on pc at the start or end of the code line. For MAC, use `command` + `return`. Alternativley, click the "Run" button at the top right of this script at each line of code.
+
+
+## 1) Install and loading the package
+pacman::p_load(ggplotgui)
+
+### Note - if above step does not work, try running this line of code below first (delete the hashtag infront before running the code)
+
+#install.packages("pacman")
+
+
+## 2) Launch the ggplot graphic user interface 
+ggplot_shiny()
